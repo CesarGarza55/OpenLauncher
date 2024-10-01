27606b96e88cb1d402fdb62282fcff869a6334de
CesarGarza55 <cesar.garza6308@alumnos.udg.mx>
2024-10-01 02:01:41 -0600

    I forgot to update the compile-windows.bat file to include the new files that were added to the project. This commit fixes that. :P

--- compile-windows.bat
+++ compile-windows.bat
@@ -1,4 +1,8 @@
 pip install -r data/requirements_windows.txt
-pyinstaller --clean --workpath ./temp --noconfirm --onefile --windowed --distpath ./ --icon "data\img\creeper.ico" --add-data "data\img;img/" "data\OpenLauncher.py"
+pyinstaller --clean --workpath ./temp --noconfirm --onefile --windowed --distpath ./ --icon "data\img\creeper.ico" ^
+    --add-data "data\img;img/" ^
+    --add-data "data\updater.py;." ^
+    --add-data "data\variables.py;." ^
+    "data\OpenLauncher.py"
 del OpenLauncher.spec
 rmdir /s /q temp
