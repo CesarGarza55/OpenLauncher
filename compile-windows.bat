3809e9096a898dcd0d004959077ac9748a3bf63a
CesarGarza55 <cesar.garza6308@alumnos.udg.mx>
2024-10-01 19:23:38 -0600

    Fix compile-windows.bat to use 'py -m pip' instead of 'pip' for installing requirements on Windows

--- compile-windows.bat
+++ compile-windows.bat
@@ -1,4 +1,4 @@
-pip install -r data/requirements_windows.txt
+py -m pip install -r data/requirements_windows.txt
 pyinstaller --clean --workpath ./temp --noconfirm --onefile --windowed --distpath ./ --icon "data\img\creeper.ico" ^
     --add-data "data\img;img/" ^
     --add-data "data\updater.py;." ^
