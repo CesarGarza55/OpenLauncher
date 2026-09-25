21f239c462bbc73f5055ee57922ae4080d6c0d9b
CesarGarza55 <cesar.garza@codevbox.com>
2026-09-25 09:08:33 -0600

    fix(ui): update Fabric loader logo to official pixel-art scroll asset

--- src/components/common/LoaderLogo.jsx
+++ src/components/common/LoaderLogo.jsx
@@ -25,16 +25,14 @@ export function MinecraftLogo({ size = 16, className = '' }) {
 
 export function FabricLogo({ size = 16, className = '' }) {
   return (
-    <svg width={size} height={size} viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
-      <path d="M16 2L4 8L16 14L28 8L16 2Z" fill="#CBD5E1" />
-      <path d="M4 8V24L16 30V14L4 8Z" fill="#94A3B8" />
-      <path d="M28 8V24L16 30V14L28 8Z" fill="#64748B" />
-      <path d="M16 6L8 10L16 14L24 10L16 6Z" fill="#38BDF8" />
-      <path d="M8 10V22L16 26V14L8 10Z" fill="#0284C7" />
-      <path d="M24 10V22L16 26V14L24 10Z" fill="#0369A1" />
-      <path d="M16 10L11 12.5L16 15L21 12.5L16 10Z" fill="#F0F9FF" />
-      <path d="M11 12.5V19.5L16 22V15L11 12.5Z" fill="#BAE6FD" />
-      <path d="M21 12.5V19.5L16 22V15L21 12.5Z" fill="#7DD3FC" />
+    <svg width={size} height={size} viewBox="0 -0.5 52 56" fill="none" xmlns="http://www.w3.org/2000/svg" className={className} shapeRendering="crispEdges">
+      <path stroke="#38342a" strokeWidth="1" d="M28 0h4m-4 1h4m-4 1h4m-4 1h4m-8 1h4m4 0h4M24 5h4m4 0h4M24 6h4m4 0h4M24 7h4m4 0h4M24 8h4m8 0h4M24 9h4m8 0h4m-16 1h4m8 0h4m-16 1h4m8 0h4m-20 1h4m4 0h4m8 0h4m-24 1h4m4 0h4m8 0h4m-24 1h4m4 0h4m8 0h4m-24 1h4m4 0h4m8 0h4m-28 1h4m12 0h4m8 0h4m-32 1h4m12 0h4m8 0h4m-32 1h4m12 0h4m8 0h4m-32 1h4m12 0h4m8 0h4m-36 1h4m20 0h4m4 0h8m-40 1h4m20 0h4m4 0h8m-40 1h4m20 0h4m4 0h8m-40 1h4m20 0h4m4 0h8M8 24h4m28 0h4m4 0h4M8 25h4m28 0h4m4 0h4M8 26h4m28 0h4m4 0h4M8 27h4m28 0h4m4 0h4M4 28h4m32 0h8M4 29h4m32 0h8M4 30h4m32 0h8M4 31h4m32 0h8M0 32h4m32 0h4M0 33h4m32 0h4M0 34h4m32 0h4M0 35h4m32 0h4M0 36h4m28 0h4M0 37h4m28 0h4M0 38h4m28 0h4M0 39h4m28 0h4M4 40h4m20 0h4M4 41h4m20 0h4M4 42h4m20 0h4M4 43h4m20 0h4M8 44h4m12 0h4M8 45h4m12 0h4M8 46h4m12 0h4M8 47h4m12 0h4m-16 1h4m4 0h8m-16 1h4m4 0h8m-16 1h4m4 0h8m-16 1h4m4 0h8m-12 1h8m-8 1h8m-8 1h8m-8 1h8" />
+      <path stroke="#dbd0b4" strokeWidth="1" d="M28 4h4m-4 1h4m-4 1h4m-4 1h4m0 1h4m-4 1h4m-4 1h4m-4 1h4m-12 1h4m8 0h4m-16 1h4m8 0h4m-16 1h4m8 0h4m-16 1h4m8 0h4m-16 1h8m8 0h4m-20 1h8m8 0h4m-20 1h8m8 0h4m-20 1h8m8 0h4m-28 1h8m4 0h8m-20 1h8m4 0h8m-20 1h8m4 0h8m-20 1h8m4 0h8m-24 1h16m-16 1h16m-16 1h16m-16 1h16M8 28h8m4 0h12M8 29h8m4 0h12M8 30h8m4 0h12M8 31h8m4 0h12M8 32h12m8 0h4M8 33h12m8 0h4M8 34h12m8 0h4M8 35h12m8 0h4m-20 1h12m-12 1h12m-12 1h12m-12 1h12m-8 1h8m-8 1h8m-8 1h8m-8 1h8" />
+      <path stroke="#c6bca5" strokeWidth="1" d="M28 8h4m-4 1h4m-4 1h4m-4 1h4m-12 5h4m-4 1h4m-4 1h4m-4 1h4m0 1h4m-4 1h4m-4 1h4m-4 1h4m0 1h8m-8 1h8m-8 1h8m-8 1h8m-20 1h4m-4 1h4m-4 1h4m-4 1h4m0 1h8m-8 1h8m-8 1h8m-8 1h8" />
+      <path stroke="#bcb29c" strokeWidth="1" d="M32 12h4m-4 1h4m-4 1h4m-4 1h4m0 1h4m-4 1h4m-4 1h4m-4 1h4m0 1h4m-4 1h4m-4 1h4m-4 1h4m-8 1h4m-4 1h4m-4 1h4m-4 1h4m-8 1h8m-8 1h8m-8 1h8m-8 1h8m-8 1h4m-4 1h4m-4 1h4m-4 1h4m-12 1h8m-8 1h8m-8 1h8m-8 1h8m-8 1h4m-4 1h4m-4 1h4m-4 1h4m-8 1h4m-4 1h4m-4 1h4m-4 1h4" />
+      <path stroke="#807a6d" strokeWidth="1" d="M44 24h4m-4 1h4m-4 1h4m-4 1h4" />
+      <path stroke="#aea694" strokeWidth="1" d="M4 32h4m-4 1h4m-4 1h4m-4 1h4m0 1h4m-4 1h4m-4 1h4m-4 1h4m0 1h4m-4 1h4m-4 1h4m-4 1h4m0 1h4m-4 1h4m-4 1h4m-4 1h4" />
+      <path stroke="#9a927e" strokeWidth="1" d="M4 36h4m-4 1h4m-4 1h4m-4 1h4m0 1h4m-4 1h4m-4 1h4m-4 1h4m0 1h4m-4 1h4m-4 1h4m-4 1h4m0 1h4m-4 1h4m-4 1h4m-4 1h4" />
     </svg>
   );
 }
